890a604086d9a2190525927fab42f2a3e16e919e
EmanShabaneh <103412143+EmanShabaneh@users.noreply.github.com>
2024-01-22 07:45:04 +0300

    recordkeeper with input validation & hash pass

--- Final_healthclinic.java
+++ Final_healthclinic.java
@@ -1,5 +1,14 @@
 package health_clinic;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -83,4 +92,278 @@ public class Final_healthclinic {
 	}
 		System.out.println("sorry :(, we are exiting ");
 	}	
-}
+
+    private static void recordkeeper() {
+		Scanner read = new Scanner(System.in);
+		System.out.println("Do you want to register a new Doctor or patient?");
+		try {
+			String userreg = read.nextLine().toLowerCase();
+			System.out.println("Registration form");
+			System.out.println("Enter name:");
+			String name = read.nextLine();
+			String gender = getgender(read);
+			int age = getage(read);
+			int phonenumber = getSanitizedPhoneNumber(read);
+			String password = getPasswordWithPolicy(read);
+
+			// Hash the password before writing it to the file
+			String hashedPassword = getHashPassword(password);
+
+			boolean flag = checkPasswordPolicy(password);
+			if (flag) {
+				System.out.println("Thank you!!!!");
+				// We want to know the file that we want to write on
+				String userfile;
+				if ("patient".equals(userreg)) {
+					userfile = "patientinfo.txt";
+				} else if ("doctor".equals(userreg)) {
+					userfile = "doctorinfo.txt";
+				} else {
+					System.out.println("Error!");
+					return;
+				}
+				boolean userExists = checkifuserexists(name, userreg);
+
+				if (userExists) {
+					System.out.println("User already exists. Please choose a different username.");
+					return; // Start again from the beginning
+				}
+
+				// Write registration info to the file
+				writeToFile(userfile, name, hashedPassword, age, gender, phonenumber);
+				System.out.println("Registration Successful");
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Input should be an integer");
+		}
+	}
+ 
+    private static final int max_gendertries = 2;
+    private static String getgender(Scanner read) {
+		int trytoinputgender = 0;
+		while (trytoinputgender < max_gendertries) {
+			try {
+				System.out.println("Enter gender male/female:");
+				String gender = read.nextLine().toLowerCase();
+				if (gender.equals("male") || gender.equals("female")) {
+					System.out.println("Gender:" + gender);
+					return gender;
+				} else {
+					System.out.println("invaled gender, please try again");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("you should enter strings");
+				read.nextLine(); // Consume the invalid input
+			}
+		}
+		return null;
+	}
+
+    private static final int max_agetries = 2;
+	private static int getage(Scanner read) {
+		int ageretrie = 0;
+		while (ageretrie < max_agetries) {
+			try {
+				System.out.println("Enter your age:");
+				int age = read.nextInt();
+				read.nextLine();
+				String agestring = Integer.toString(age);
+				if (agestring.length() <= 3) {
+					System.out.println("Age: " + age);
+					return age;
+				} else {
+					System.out.println("Invalid age. Please try again.");
+					ageretrie++;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("you should enter strings");
+				read.nextLine(); // Consume the invalid input
+			}
+		}
+		return 0;
+	}
+
+	private static int getSanitizedPhoneNumber(Scanner read) {
+		int phonenumber;
+		while (true) {
+			try {
+				System.out.println("Enter phone number:");
+				phonenumber = read.nextInt();
+				read.nextLine(); // Consume the newline character
+				if (String.valueOf(phonenumber).length() == 10) {
+					break;
+				} else {
+					System.out.println("Invalid phone number. Please enter a 10-digit number.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("you should enter an integer number.");
+				read.nextLine(); // Consume the invalid input
+			}
+			catch (NumberFormatException e) {
+				System.out.println("bbb");
+			}
+		}
+		return phonenumber;
+	}
+
+	private static String getPasswordWithPolicy(Scanner read) {
+		String password;
+		while (true) {
+			System.out.println("Enter password (at least 10 characters, including uppercase, lowercase, and a digit):");
+			password = read.nextLine();
+			if (checkPasswordPolicy(password)) {
+				break;
+			} else {
+				System.out.println("sorry, Please ensure it meets the policy requirements.");
+			}
+		}
+		return password;
+	}
+
+	private static boolean checkPasswordPolicy(String password) {
+		return password.length() >= 10 && password.matches(".*[A-Z].*") && password.matches(".*[a-z].*")
+				&& password.matches(".*\\d.*");
+	}
+
+	private static void writeToFile(String userfile, String name, String password, int age, String gender,
+			int phonenumber) {
+		BufferedWriter bw = null;
+		try {
+			// create the BufferedWriter
+			bw = new BufferedWriter(new FileWriter(userfile, true));
+			try {
+				bw.write(name + "," + password + "," + age + "," + gender + "," + phonenumber + "\n");
+				System.out.println("Data written successfully");
+			} finally {
+				bw.close();
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	private static boolean checkifuserexists(String enteredUsername, String role) {
+		String fileName = role + "info.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] userInfo = line.split(",");
+				if (userInfo.length >= 1) {
+					String storedUsername = userInfo[0].trim();
+					if (enteredUsername.equals(storedUsername)) {
+						return true; // User already exists
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		return false; // User does not exist
+	}
+
+	private static String getHashPassword(String password) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			password = encode(md.digest(password.getBytes(StandardCharsets.UTF_8)));
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("The algorithm does not exist");
+		}
+		return password;
+	}
+
+	private static String encode(byte[] digest) {
+		return Base64.getEncoder().encodeToString(digest);
+	}
+
+	private static boolean checkcredentialsrecordkeeper(String enteredUsername, String enteredPassword) {
+		try (BufferedReader br = new BufferedReader(new FileReader("recordkeeperinfo.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				// Split the line into username and password
+				String[] users = line.split(",");
+				if (users.length == 2) {
+					String storedUsername = users[0].trim();
+					String storedPassword = users[1].trim();
+
+					// Hash the entered password for comparison
+					String Passwordentered = getHashPassword(enteredPassword);
+
+					// Check if the entered credentials match any records
+					if (enteredUsername.equals(storedUsername) && Passwordentered.equals(storedPassword)) {
+						return true;
+					}
+				} else {
+					System.out.println("Invalid username or password");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file");
+		}
+		return false;
+	}
+
+	private static boolean checkcredentialspatient(String enteredUsername, String enteredPassword) {
+		try (BufferedReader br = new BufferedReader(new FileReader("patientinfo.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				// Split the line into username and password
+				String[] users = line.split(",");
+				if (users.length == 5) {
+					String storedUsername = users[0].trim();
+					String storedPassword = users[1].trim();
+
+					// Hash the entered password for comparison
+					String Passwordentered = getHashPassword(enteredPassword);
+
+					// Check if the entered credentials match any records
+					if (enteredUsername.equals(storedUsername) && Passwordentered.equals(storedPassword)) {
+						return true;
+					}
+				} 
+				else {
+					System.out.println("Invalid username or password");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file");
+		}
+		return false;
+	}
+
+	private static boolean checkcredentialsdoctor(String enteredUsername, String enteredPassword) {
+		try (BufferedReader br = new BufferedReader(new FileReader("doctorinfo.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				// Split the line into username and password
+				String[] users = line.split(",");
+				if (users.length == 5) {
+					String storedUsername = users[0].trim();
+					String storedPassword = users[1].trim();
+
+					// Hash the entered password for comparison
+					String Passwordentered = getHashPassword(enteredPassword);
+
+					// Check if the entered credentials match any records
+					if (enteredUsername.equals(storedUsername) && Passwordentered.equals(storedPassword)) {
+						return true;
+					}
+				} else {
+					System.out.println("Invalid username or password");
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file");
+		}
+		return false;
+	}
+	
+
+
+
+
+
+
+
+
+}	
